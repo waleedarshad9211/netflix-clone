6542094cedcf4973c4c1f6342e6422dbe8eadfb2
waleed arshad <waleed.arshad.saeed@gmail.com>
2021-05-25 16:12:06 +0500

    shows data on screen with api call

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
       <h1>Hello there We are creating a netflix clone today!</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
+      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated}/>
+      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,24 +1,37 @@
 import React, {useState,useEffect} from 'react';
 import axios from './axios';
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl}){
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-         
         async function fetchData(){
             const request = await axios.get(fetchUrl);
-            console.log(request);
+
+            setMovies(request.data.results);
+            console.log("Helooooooooooo",request)
             return request;
         }
         fetchData();
-    },[fetchUrl]);
-
+    },[fetchUrl])
 
     return(
-        <div>
-            <h2>{title}</h2>
-            {}
+        <div className="row">
+             <h2>{title}</h2>
+            <div className="row__posters">
+                
+                {movies.map(movie => (
+                    <img
+                        className="row__poster" 
+                        src={base_url+movie.poster_path} 
+                        alt = {movie.name}
+                    />
+                ))}
+                    
+            </div>
         </div>
     )
 }
