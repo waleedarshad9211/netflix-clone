a1e7f3823085a908dde15488d97bba0fed1645a2
waleed arshad <waleed.arshad.saeed@gmail.com>
2021-05-25 23:06:59 +0500

    A full fledge working Home Page of netflix clone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import requests from './requests';
 import Row from './Row';
+import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello there We are creating a netflix clone today!</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Nav />
+      <Banner />
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals} 
+        isLargeRow={true}
+      />
       <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
       <Row title="TOP RATED" fetchUrl={requests.fetchTopRated}/>
       <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies}/>
