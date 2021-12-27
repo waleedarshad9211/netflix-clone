dcfb6d7b3cfc911870de3fd33f65ec9df2ed48e7
waleed arshad <waleedarshad.leyjao@gmail.com>
2021-12-27 21:47:12 +0500

    data

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@
 
 const requests = {
     fetchTrending: '/trending/all/day?api_key=c660986ae70982029ef7dad7f4b2dc52',
-    fetchNetflixOriginals: '/discover/tv?api_key=c660986ae70982029ef7dad7f4b2dc52&with_network=213',
+    fetchNetflixOriginals: '/discover/tv?api_key=c660986ae70982029ef7dad7f4b2dc52&with_networks=213',
     fetchTopRated: '/movie/top_rated?api_key=c660986ae70982029ef7dad7f4b2dc52&language=en-US',
     fetchActionMovies: '/discover/movie?api_key=c660986ae70982029ef7dad7f4b2dc52&with_genres=28',
     fetchComedyMovies: '/discover/movie?api_key=c660986ae70982029ef7dad7f4b2dc52&with_genres=35',
